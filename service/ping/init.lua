5c8120273a16c909b188ea11f24d27f072f1889d
Franking502 <867881045@qq.com>
2024-07-23 00:33:05 -0700

    通过lua调用服务

--- service/ping/init.lua
+++ service/ping/init.lua
@@ -6,14 +6,24 @@ function OnInit(id)
 end
 
 function OnServiceMsg(source, buff)
-    print("[lua] ping OnServiceMsg id:"..serviceId)
+    --参数序列化
+    local n1 = 0
+    local n2 = 0
 
-    if string.len(buff) > 50 then
-        sunnet.KillService(serviceId)
-        return
+    --解码
+    if buff ~= "start" then
+        n1, n2 = string.unpack("i4 i4", buff)
     end
 
-    sunnet.Send(serviceId, source, buff.."i")
+    --处理
+    print("[lua] ping OnServiceMsg n1:"..n1.."n2:"..n2)
+    n1 = n1 + 1
+    n2 = n2 + 1
+
+    --编码
+    buff = string.pack("i4 i4", n1, n2)
+    
+    sunnet.Send(serviceId, source, buff)
 end
 
 function OnExit()
--- src/LuaAPI.cpp
+++ src/LuaAPI.cpp
@@ -13,9 +13,9 @@ void LuaAPI::Register(lua_State *luaState)
         {"NewService", NewService},
         {"KillService", KillService},
         {"Send", Send},
-        //{"Listen", Listen},
-        //{"CloseConn", CloseConn},
-        //{"Write", Write},
+        {"Listen", Listen},
+        {"CloseConn", CloseConn},
+        {"Write", Write},
         {NULL, NULL}
     };
     luaL_newlib(luaState, lualibs);
@@ -61,6 +61,7 @@ int LuaAPI::KillService(lua_State *luaState)
 
 int LuaAPI::Send(lua_State *luaState)
 {
+    //参数个数
     int num = lua_gettop(luaState);
     if(num != 3)
     {
@@ -105,4 +106,86 @@ int LuaAPI::Send(lua_State *luaState)
     Sunnet::inst->Send(toId, msg);
 
     return 0;
+}
+
+//开启网络监听
+int LuaAPI::Listen(lua_State *luaState)
+{
+    //参数个数
+    int num = lua_gettop(luaState);
+    if(num != 2)
+    {
+        cout << "Send fail, need 2 args, passed " << num << endl;
+        return 0;
+    }
+
+    //参数1：端口
+    if(lua_isinteger(luaState, 1) == 0) {
+        lua_pushinteger(luaState, -1);
+        return 1;
+    }
+    int port = lua_tointeger(luaState, 1);
+
+    //参数2：服务Id
+    if(lua_isinteger(luaState, 2) == 0) {
+        lua_pushinteger(luaState, -1);
+        return 1;
+    }
+    int id = lua_tointeger(luaState, 2);
+
+    //处理
+    int fd = Sunnet::inst->Listen(port, id);
+
+    //返回值
+    lua_pushinteger(luaState, fd);
+    return 1;
+}
+
+//关闭连接
+int LuaAPI::CloseConn(lua_State *luaState)
+{
+    //参数个数
+    int num = lua_gettop(luaState);
+
+    //参数1：fd
+    if(lua_isinteger(luaState, 1) == 0) {
+        return 0;
+    }
+    int fd = lua_tointeger(luaState, 1);
+
+    //处理
+    Sunnet::inst->CloseConn(fd);
+
+    //返回值
+    //（无）
+    return 0;
+}
+
+//写套接字
+int LuaAPI::Write(lua_State *luaState)
+{
+    //参数个数
+    int num = lua_gettop(luaState);
+
+    //参数1：fd
+    if(lua_isinteger(luaState, 1) == 0) {
+        lua_pushinteger(luaState, -1);
+        return 1;
+    }
+    int fd = lua_tointeger(luaState, 1);
+
+    //参数2：buff
+    if(lua_isstring(luaState, 2) == 0){
+        lua_pushinteger(luaState, -1);
+        return 1;
+    }
+    size_t len = 0;
+    const char *buff = lua_tolstring(luaState, 2, &len);
+
+    //处理
+    int r = write(fd, buff, len);
+    
+    //返回值
+    lua_pushinteger(luaState, r);
+    return 1;
 }
